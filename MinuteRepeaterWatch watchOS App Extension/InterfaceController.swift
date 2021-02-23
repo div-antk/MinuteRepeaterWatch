29a2ed526e49e4f90b047a8c817cacafe7600e2d
div-antk <51283468+owaii@users.noreply.github.com>
2021-02-23 18:02:39 +0900

    リピーター機能の実装

--- MinuteRepeaterWatch watchOS App Extension/InterfaceController.swift
+++ MinuteRepeaterWatch watchOS App Extension/InterfaceController.swift
@@ -7,11 +7,18 @@
 
 import WatchKit
 import Foundation
-
+import AVFoundation
 
 class InterfaceController: WKInterfaceController {
 
     @IBOutlet var skInterface: WKInterfaceSKScene!
+    
+    // ゴング音
+    private var highGongPlayer:AVAudioPlayer?
+    private var lowGongPlayer:AVAudioPlayer?
+
+    // 再生中かどうか
+    private var isPlaying = false
     
     override func awake(withContext context: Any?) {
         super.awake(withContext: context)
@@ -35,4 +42,91 @@ class InterfaceController: WKInterfaceController {
         super.didDeactivate()
     }
 
+    // 再生ボタン
+    @IBAction func startButton() {
+        if isPlaying { return }
+        isPlaying = true
+        
+        repeater()
+    }
+    
+    // リピーター機能
+    func repeater() {
+        
+        DispatchQueue.global().async {
+            
+            let now = Date()
+            let time = Calendar.current.dateComponents([.hour, .minute], from: now)
+            
+            // hour を12時間で丸める
+            let hour = time.hour! % 12
+            let quarter = time.minute! / 15
+            let minute = time.minute! % 15
+            
+            // 第一ゴング
+            for _ in 0 ..< hour {
+                print("dong")
+                self.lowGong()
+                // 0.5秒待つ
+                Thread.sleep(forTimeInterval: 0.5)
+            }
+            
+            // 第二ゴングが鳴る場合遅延処理
+            if quarter > 0 {
+                Thread.sleep(forTimeInterval: 0.7)
+            }
+            
+            // 第二ゴング
+            for _ in 0 ..< quarter {
+                print("ding-dong")
+                self.highGong()
+                // 0.4秒待つ
+                Thread.sleep(forTimeInterval: 0.4)
+                self.lowGong()
+                
+                // 0.5秒待つ
+                Thread.sleep(forTimeInterval: 0.5)
+            }
+            
+            // 第三ゴングが鳴る場合遅延処理
+            if minute > 0 {
+                Thread.sleep(forTimeInterval: 0.7)
+            }
+            
+            // 第三ゴング
+            for _ in 0 ..< minute {
+                print("ding")
+                self.highGong()
+                
+                // 0.5秒待つ
+                Thread.sleep(forTimeInterval: 0.5)
+            }
+            self.isPlaying = false
+        }
+    }
+    
+    // 低音
+    func lowGong() {
+        
+        let lowGong = Bundle.main.url(forResource: "low", withExtension: "wav")
+        
+        do {
+            lowGongPlayer = try AVAudioPlayer(contentsOf: lowGong!)
+            lowGongPlayer?.play()
+        } catch {
+            print("error")
+        }
+    }
+    
+    // 高音
+    func highGong() {
+        
+        let highGong = Bundle.main.url(forResource: "high", withExtension: "wav")
+        do {
+            highGongPlayer = try AVAudioPlayer(contentsOf: highGong!)
+            highGongPlayer?.play()
+        } catch {
+            print("error")
+        }
+    }
 }
